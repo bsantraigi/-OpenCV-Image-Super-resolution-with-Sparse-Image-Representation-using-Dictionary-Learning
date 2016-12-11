e7c26fd0118cc4dec0bc05f7262845b069b0952e
Bishal Santra <bs.bytestorm@gmail.com>
2016-12-11 23:31:48 +0530

    Betarnd added

--- MTP_Port2/MainS.cpp
+++ MTP_Port2/MainS.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 Timer timer;
 
+typedef Eigen::Matrix<bool, -1, -1> MatrixXb;
+
 int main( int argc, char** argv )
 {	
 	ImLoader imloader(8, 4);
@@ -21,8 +23,13 @@ int main( int argc, char** argv )
 	timer.start();
 
 	DLConfig config1;
-	
-	DLLayer layer1(fMat, config1);
+	MatrixXb B(2, 2);
+	B << 1, 0, 0, 1;
+	cout << "B" << endl << B << endl;
+	B *= MatrixXb::Ones(2, 2);
+	cout << "B" << endl << B << endl;
+	cout << "B" << endl << B << endl;
+	//DLLayer layer1(fMat, config1);
 
 	timer.stop();
 	
